f5d4579ac82bf726bd45db1b4be5edc280ae44cd
trynyen <tryn.yen@gmail.com>
2019-06-25 19:20:12 -0500

    fixing Find rotten Tomatoes

--- liri.js
+++ liri.js
@@ -1,49 +1,69 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-//  require the spotify package
+var axios = require("axios");
+var fs = require("fs");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-var axios = require("axios");
-var fs = require("fs");
 
 //Spotify search
 if (process.argv[2] == "spotify-this-song") {
-    var track = process.argv.slice(3).join(" ")
+    var trackSearch = process.argv.slice(3).join(" ");
+
+    if (trackSearch == undefined || null) {
+        trackSearch = "Aggretsuko Theme";
+    }
+
     spotify.search({
         type: "track",
-        query: track,
+        query: trackSearch,
         limit: 1,
     }, function (err, data) {
         if (err) {
             return console.log(err);
         }
-        console.log(JSON.stringify(data.tracks.items[0].name));
-
+        var trackDetails = data.tracks.items;
+        for (var i = 0; i < trackDetails.length; i++) {
+            console.log("------------------------------------------------------------");
+            console.log("Artist(s): " + trackDetails[i].artists[0].name);
+            console.log("Song: " + trackDetails[i].name);
+            console.log("Preview track: " + trackDetails[i].preview_url);
+            console.log("Album: " + trackDetails[i].album.name);
+            console.log("------------------------------------------------------------");
+        }
     }
     )
 }
 
 //OMDB search
-if (process.argv[2] == "movie-this") {
-    var movie = process.argv.slice(3).join(" ")
+else if (process.argv[2] == "movie-this") {
+    var movieSearch = process.argv.slice(3).join(" ")
 
 
-    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
+    axios.get("http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
-            // Then we print out the imdbRating
+            //Search for Rotten Tomatoes Ratings
+            function rottenTomatoesRating() {
+                response.data.Ratings.find(function (rating) {
+                    if (rating.Source === "Rotten Tomatoes") {
+                        return rating.Source.Value;
+                    }
+                    else {
+                        return "Rotten Tomatoes Rating is not available";
+                    }
+                })
+            }
 
+            //Log Movie Details
             console.log("Movie title: " + response.data.Title);
             console.log("Year: " + response.data.Year);
             console.log("imdb rating: " + response.data.imdbRating);
-
-            //Need sto find rotten tomatoes first
-            console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+            console.log("Rotten Tomatoes rating: " + rottenTomatoesRating());
             console.log("Country: " + response.data.Country);
             console.log("Language: " + response.data.Language);
             console.log("Plot: " + response.data.Plot);
             console.log("Cast: " + response.data.Actors);
-        }
+        },
     )
 };
 
